a6f71a7e62464ef16e961492deae2be12664c7ce
Ferryyansyah <ferryyansyah10@gmail.com>
2017-02-21 11:18:18 +0700

    clueiiijkr

--- resources/views/profilpegawai.blade.php
+++ resources/views/profilpegawai.blade.php
@@ -7,28 +7,28 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.5 -->
-    <link rel="{{url('stylesheet" href="/bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{url('/bootstrap/css/bootstrap.min.css')}}">
     <!-- Font Awesome -->
-    <link rel="{{url('stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{url('https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css')}}">
     <!-- Ionicons -->
-    <link rel="{{ur('stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css')}}">
+    <link rel="stylesheet" href="{{url('https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css')}}">
     <!-- Theme style -->
-    <link rel="{{url('stylesheet" href="/dist/css/AdminLTE.min.css')}}">
+    <link rel="stylesheet" href="{{url('/dist/css/AdminLTE.min.css')}}">
     <!-- AdminLTE Skins. Choose a skin from the css/skins
          folder instead of downloading all of them to reduce the load. -->
-    <link rel="{{url('stylesheet" href="/dist/css/skins/_all-skins.min.css')}}">
+    <link rel="stylesheet" href="{{url('/dist/css/skins/_all-skins.min.css')}}">
     <!-- iCheck -->
-    <link rel="{{url('stylesheet" href="/plugins/iCheck/flat/blue.css')}}">
+    <link rel="stylesheet" href="{{url('/plugins/iCheck/flat/blue.css')}}">
     <!-- Morris chart -->
-    <link rel="{{url('stylesheet" href="/plugins/morris/morris.css')}}">
+    <link rel="stylesheet" href="{{url('/plugins/morris/morris.css')}}">
     <!-- jvectormap -->
-    <link rel="{{url('stylesheet" href="/plugins/jvectormap/jquery-jvectormap-1.2.2.css')}}">
+    <link rel="stylesheet" href="{{url('/plugins/jvectormap/jquery-jvectormap-1.2.2.css')}}">
     <!-- Date Picker -->
-    <link rel="{{url('stylesheet" href="/plugins/datepicker/datepicker3.css')}}">
+    <link rel="stylesheet" href="{{url('/plugins/datepicker/datepicker3.css')}}">
     <!-- Daterange picker -->
-    <link rel="{{url('stylesheet" href="/plugins/daterangepicker/daterangepicker-bs3.css')}}">
+    <link rel="stylesheet" href="{{url('/plugins/daterangepicker/daterangepicker-bs3.css')}}">
     <!-- bootstrap wysihtml5 - text editor -->
-    <link rel="{{url('stylesheet" href="/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css')}}">
+    <link rel="stylesheet" href="{{url('/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css')}}">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -436,7 +436,7 @@
     <!-- Bootstrap WYSIHTML5 -->
     <script src="{{url('plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js')}}"></script>
     <!-- Slimscroll -->
-    <script src="{{url('plugins/slimScroll/jquery{{url('.slimscroll.min.js')}}"></script>
+    <script src="{{url('plugins/slimScroll/jquery.slimscroll.min.js')}}"></script>
     <!-- FastClick -->
     <script src="{{url('plugins/fastclick/fastclick.min.js')}}"></script>
     <!-- AdminLTE App -->
@@ -446,4 +446,4 @@
     <!-- AdminLTE for demo purposes -->
     <script src="{{url('dist/js/demo.js')}}"></script>
   </body>
-</html>{{url('{{url('
+</html>
